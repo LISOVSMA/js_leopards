b060fecd5dbbc52f4d0cea3fe45045ae9e93fdc2
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-01-27 20:22:47 +0200

    add task1

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// 1. Напишіть програму, яка запитає у користувача "Enter the number of minutes to format"
+
+//число (кількість хвилин) і виведе в консоль
+
+//рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+// const globalMinutes = 70;
+// const hours = Math.floor(globalMinutes / 60);
+// const modificateHours = String(hours).padStart(2, 0);
+// const minutes = globalMinutes % 60;
+// const modificateMinutes = String(minutes).padStart(2, 0);
+// const time = `${modificateHours}:${modificateMinutes}`;
+// console.log(time);
