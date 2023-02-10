a5c14b5daaa19ae8d6b23d5ad5e9ff021545d2bd
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-02-10 22:35:09 +0200

    adds task

--- js/lesson-2/iteration.js
+++ js/lesson-2/iteration.js
@@ -1,15 +1,14 @@
-'use strict';
+"use strict";
 
 /**
   |============================
   | for...in, Object.keys, Object.values
   |============================
 */
-const salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
-};
+
+// salaries.Maria = 500;
+
+// console.log(Object.values(salaries));
 
 //! Проблема for in
 // hasOwnProperty
@@ -21,10 +20,43 @@ const salaries = {
 // console.log(obj);
 
 // for (const employeeName in obj) {
-//   if (obj.hasOwnProperty(employeeName)) {
-//     console.log(employeeName);
-//   }
+//   // if (obj.hasOwnProperty(employeeName)) {
+//   //   console.log(employeeName);
+//   // }
+//   // console.log(employeeName);
 // }
 
 //TODO:==========================
 //? Виконати завдання - Отримайте суму зарплат усіх працівників
+
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// let total = 0;
+
+// Варіант 1:
+
+// for (const item in salaries) {
+//   console.log(item);
+//   total += salaries[item];
+// }
+
+// Варіант 2:
+
+// const values = Object.values(salaries);
+// // console.log(values);
+// for (const value of values) {
+//   total += value;
+// }
+
+// Варіант 3:
+// const keys = Object.keys(salaries);
+// console.log(keys);
+// for (const key of keys) {
+//   total += salaries[key];
+// }
+
+// console.log(total);
--- js/lesson-2/object-pattern.js
+++ js/lesson-2/object-pattern.js
@@ -1,28 +1,42 @@
-'use strict';
+"use strict";
 
-const logFunction = function (user) {
-  const { firstName, lastName, age, gender, userEmail, userPhoneNumber } = user;
+// const logFunction = function (user) {
+//   const {
+//     firstName,
+//     lastName,
+//     age = 40,
+//     gender,
+//     userEmail,
+//     userPhoneNumber,
+//   } = user;
 
-  console.log(firstName, lastName, age, gender, userEmail, userPhoneNumber);
-};
+//   console.log(firstName, lastName, age, gender, userEmail, userPhoneNumber);
+// };
 
-// logFunction('Amelia', 'Burgess', 30, 'female', 'apjez@eh.aq', '(096) 35-21-476');
-// logFunction();
+// logFunction(
+//   "Amelia",
+//   "Burgess",
+//   30,
+//   "female",
+//   "apjez@eh.aq",
+//   "(096) 35-21-476"
+// );
+// // logFunction();
 
 // logFunction({
-//   firstName: 'Amelia',
-//   lastName: 'Burgess',
+//   firstName: "Amelia",
+//   lastName: "Burgess",
 //   age: 30,
-//   gender: 'female',
-//   userEmail: 'apjez@eh.aq',
-//   userPhoneNumber: '(096) 35-21-476',
+//   gender: "female",
+//   userEmail: "apjez@eh.aq",
+//   userPhoneNumber: "(096) 35-21-476",
 // });
 
 // logFunction({
-//   firstName: 'Hunter',
-//   lastName: 'Hicks',
+//   firstName: "Hunter",
+//   lastName: "Hicks",
 //   age: 40,
-//   gender: 'male',
-//   userEmail: 'test@eh.aq',
-//   userPhoneNumber: '(096) 45-21-476',
+//   gender: "male",
+//   userEmail: "test@eh.aq",
+//   userPhoneNumber: "(096) 45-21-476",
 // });
